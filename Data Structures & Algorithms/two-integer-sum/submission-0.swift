5a389853e74324a57d79bb9b392613e8d49ad456
Kushagra Sinha <74058421+KushagraSinha2002@users.noreply.github.com>
2026-08-12 10:52:57 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        // Create a Dict[value,index]
+        //loop over nums and them to Dict
+        // in same loop do dict[target-nums[i]] != nil {return [dict[target-nums[i], nums[i]]]}
+        
+        var numIndexRecord: [Int:Int] = [:]
+
+        for i in 0..<nums.count{
+            if let index = numIndexRecord[target-nums[i]] {
+                return [index, i]
+            }
+            numIndexRecord[nums[i], default: 0] = i
+        }
+        return []
+    }
+}
